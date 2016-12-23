7e15a8804d5e6809db240b0cfc320fb48c24f148
pkatira7 <pkatira@vmware.com>
2016-12-23 15:01:42 +0530

    Add files via upload

---
+++ AdditionTwoNos.java
@@ -0,0 +1,16 @@
+
+public class AdditionTwoNos {
+public static void main (String [] args)
+{
+	
+	int num1 =5 , num2 = 7 ;
+	int count = 1 ;
+	while ( count <=num2 )
+	{
+		 num1 ++ ; 
+	    count++ ; 
+	}
+	System.out.println(num1) ; 
+	
+}
+}
